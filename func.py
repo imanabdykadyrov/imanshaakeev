c12b1447ee443186a157ce5f3e2f5326d4861640
imanabdykadyrov <iman_abdykadyrov@icloud.com>
2021-07-16 19:17:29 +0600

    edit func.py

--- func.py
+++ func.py
@@ -6,5 +6,15 @@ def num(nums):
             odd.append(i)
         else:
             even.append(i)
+
+def num(nums):
+    odd = []
+    even = []
+    for i in nums:
+        if i % 2 == 0:
+            odd.append(i)
+        else:
+            even.append(i)
+            
 my_l = [1,2,3,4,5,6,7,8,9]
 print(num(my_l))
