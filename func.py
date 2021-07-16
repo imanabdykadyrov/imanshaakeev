604a02562343101a5bdd8e476be0402f7d21b7b4
imanabdykadyrov <iman_abdykadyrov@icloud.com>
2021-07-16 18:59:41 +0600

    second commit

---
+++ func.py
@@ -0,0 +1,10 @@
+def num(nums):
+    odd = []
+    even = []
+    for i in nums:
+        if i % 2 == 0:
+            odd.append(i)
+        else:
+            even.append(i)
+my_l = [1,2,3,4,5,6,7,8,9]
+print(num(my_l))
